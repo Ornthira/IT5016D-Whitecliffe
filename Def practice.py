1c8ce8b7eda53d6f08a52b6d3452603d877cd513
Ornthira <128682104+Ornthira@users.noreply.github.com>
2023-05-07 19:37:25 +1200

    Add files via upload

---
+++ Def practice.py
@@ -0,0 +1,12 @@
+#example illustrating functions
+def SmallTalk():
+    print("How you doin?")
+    print("You good?")
+#callng function
+SmallTalk()
+
+def Response():
+    got_a_bite = input("Tell me you're good..")
+    if "good" in got_a_bite:
+        print ("What do ya say? say we make like a tree and leaf?")
+Response()
