9d4b711ec4cece0fc45b008aad02d22d85dc5cbb
Ornthira <128682104+Ornthira@users.noreply.github.com>
2023-05-09 21:17:12 +1200

    For in loop continue exercise
    Adapted and manipulated from https://www.simplilearn.com/tutorials/python-tutorial/python-for-loop

---
+++ For in loop continue practice.py
@@ -0,0 +1,7 @@
+colors = ["Orange", "Yellow", "Green", "Pink", "Blue"]
+for x in colors:
+    print(x)
+    if x == "Green":
+        continue
+    else:
+        print ("Is that all the colors you can think of?")
