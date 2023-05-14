65c452f9c318a736324c7e53a34ff6f6f7e6e918
Ornthira <128682104+Ornthira@users.noreply.github.com>
2023-05-14 22:26:21 +1200

    Update menu - practice.py

--- menu - practice.py
+++ menu - practice.py
@@ -8,3 +8,5 @@ print("4: Re-open resolved ticket\n")
 print("5: Display ticket stats\n")
 print("----------------------------------\n")
 menu_selection = int(input("Enter menu selection 0 - 5 :\n"))
+#I learnt to further simplify this by compacting some of the print lines to 
+#print("0: Exit\n1: Submit helpdesk ticket\n2: Show all tickets\n3: Respond to ticket by number")
