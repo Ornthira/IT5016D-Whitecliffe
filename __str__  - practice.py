962468d86c6385d7886c8fc1de170519f08ea5f4
Ornthira <128682104+Ornthira@users.noreply.github.com>
2023-05-14 22:21:38 +1200

    Update __str__  - practice.py

--- __str__  - practice.py
+++ __str__  - practice.py
@@ -1,14 +1,15 @@
 #Part of this code is taken from an example I found online https://realpython.com/lessons/how-and-when-use-str/ 
 #when I was trying to understand and find pratical examples of classes, def,__init__ and __str__.
 #to help with my learning I extended the code by building on it.
+#creating class object and setting it's attributes.
 class Car:
     def __init__(self, color, mileage):
         self.color = color
         self.mileage = mileage
-
+#assigning the format that the data will be presented.
     def __str__(self):
         return "You have a {} car that has done {} miles.".format(self.color, self.mileage)
-#  
+#defining the method
 def MyCar():
     color = input("Car Color:")
     mileage = int(input("Mileage:"))
@@ -16,6 +17,7 @@ def MyCar():
     print(my_car)
 
 print ("Enter details of your car")
+#calling the function
 my_car = MyCar()
 
 
