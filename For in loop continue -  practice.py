9775b44d84b25ea6af8839fc4b786656349b7e60
Ornthira <128682104+Ornthira@users.noreply.github.com>
2023-05-14 22:16:01 +1200

    Update For in loop continue -  practice.py

--- For in loop continue -  practice.py
+++ For in loop continue -  practice.py
@@ -1,8 +1,10 @@
 #practice code for understanding list, for loop and == 
 #own code
+#assigning a list to a variable.
 colors = ["Orange", "Yellow", "Green", "Pink", "Blue"]
 for x in colors:
     print(x)
+# passing methond for checking if something is something (in this case if x is green)
     if x == "Green":
         continue
     else:
